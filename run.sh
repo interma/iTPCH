68ee9ba0585d627f0c6a9682177c27c3a8237824
interma <interma@outlook.com>
2024-02-08 11:00:42 +0800

    add script and sql

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+cd dbgen
+
+# 01 compile dbgen
+make
+
+# 02 generate raw data
+./dbgen -vf -s 1
+# and remove ending |
+sed -i -e "s/|$//g" *.tbl
+
+# 03 import data via copy command
+psql tpch -c "\\COPY nation FROM 'nation.tbl' WITH DELIMITER '|';"
+psql tpch -c "\\COPY part FROM 'part.tbl' WITH DELIMITER '|';"
+psql tpch -c "\\COPY customer FROM 'customer.tbl' WITH DELIMITER '|';"
+psql tpch -c "\\COPY orders FROM 'orders.tbl' WITH DELIMITER '|';"
+psql tpch -c "\\COPY partsupp FROM 'partsupp.tbl' WITH DELIMITER '|';"
+psql tpch -c "\\COPY region FROM 'region.tbl' WITH DELIMITER '|';"
+psql tpch -c "\\COPY supplier FROM 'supplier.tbl' WITH DELIMITER '|';"
+psql tpch -c "\\COPY lineitem FROM 'lineitem.tbl' WITH DELIMITER '|';"
+
+cd ..
+# 04 run 22 queries
+psql tpch -f query.sql > result.txt
+
