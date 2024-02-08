512fbf93c40a6bf1af11a79fc6a94e57d2beffc9
interma <interma@outlook.com>
2024-02-08 15:07:29 +0800

    fix query 15

--- query.sql
+++ query.sql
@@ -5,7 +5,7 @@
 -- enable ORCA
 -- set optimizer to on;
 
--- 0
+-- 0, just to see how many rows here
 select count(*) from lineitem;
 
 -- 1
@@ -395,6 +395,17 @@ where
  and l_shipdate >= date '1997-04-01'
  and l_shipdate < date '1997-04-01' + interval '1 month';
 -- 15
+with revenue_view as (
+  select
+    l_suppkey as supplier_no,
+    sum(l_extendedprice * (1 - l_discount)) as total_revenue
+  from
+    lineitem
+  where
+    l_shipdate >= '1996-01-01'
+    and l_shipdate < '1996-04-01'
+  group by
+    l_suppkey)
 select
  s_suppkey,
  s_name,
@@ -403,14 +414,14 @@ select
  total_revenue
 from
  supplier,
- revenue
+ revenue_view
 where
  s_suppkey = supplier_no
  and total_revenue = (
  select
  max(total_revenue)
  from
- revenue
+ revenue_view
  )
 order by
  s_suppkey;
